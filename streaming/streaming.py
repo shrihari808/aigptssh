6950907aeae6fedcb563f3fecf56ad847b0839c7
Shrihari Kulkarni <kshrihari0611@gmail.com>
2025-09-04 12:44:40 +0530

    4o quality better :(

--- streaming/streaming.py
+++ streaming/streaming.py
@@ -715,6 +715,9 @@ async def web_rag_mix(
             """
         )
         final_chain = final_prompt | llm_stream
+        # Initialize the Gemini Flash model
+        # gemini_flash = ChatGoogleGenerativeAI(model="models/gemini-2.5-pro", convert_system_message_to_human=True)
+        # final_chain = final_prompt | gemini_flash
 
         final_response_text = ""
         disclaimer = DISCLAIMER_TEXT
